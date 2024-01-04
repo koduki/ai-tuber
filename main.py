3eb15fec1427078ef5b53c73781f77b991ed599b
koduki <pascalm3+git@gmail.com>
2024-01-05 04:14:17 +0900

    Add output_parser

--- main.py
+++ main.py
@@ -9,25 +9,44 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate
 )
+from typing import List
+
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+class Reply(BaseModel):
+    current_emotion: str = Field(description="maxe")
+    character_reply: str = Field(description="あい's reply to User")
+
+parser = JsonOutputParser(pydantic_object=Reply)
+
 
 prompt_system = open("prompt_system.txt", "r", encoding='utf-8').read()
 
-prompt = ChatPromptTemplate.from_messages([
-    SystemMessagePromptTemplate.from_template(prompt_system),
-    MessagesPlaceholder(variable_name="history"),
-    HumanMessagePromptTemplate.from_template("{input}")
-])
+# prompt = ChatPromptTemplate.from_messages([
+#     SystemMessagePromptTemplate.from_template(prompt_system),
+#     MessagesPlaceholder(variable_name="history"),
+#     HumanMessagePromptTemplate.from_template("{input}"),
+# ])
 
+from langchain.prompts import ChatPromptTemplate
+prompt = ChatPromptTemplate.from_messages([
+    ("system", prompt_system),
+    ("user", "{input}")
+])
 
 # チャットチェーンの準備
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
-#llm = ChatOpenAI(temperature=0)
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
 llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-memory = ConversationBufferMemory(return_messages=True)
-conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
+# memory = ConversationBufferMemory(return_messages=True)
+# conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, partial_variables={"format_instructions": parser.get_format_instructions()})
+chain = prompt | llm | parser
+
 
 import pytchat
 
@@ -49,11 +68,25 @@ print("Ready. Youtubeにコメントしてね。")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"{c.datetime} [{c.author.name}]: {c.message}")
-        res = conversation.predict(input=c.message)
+        res = chain.invoke({"input": c.message, "format_instructions": parser.get_format_instructions()})
+        print("s00")
         print(res)
-        reply = json.loads(res)
-        
+        print("s01")
+        try:
+            # print(parser.parse(res))
+            print("s02")
+            data = str(res).replace("'", "\"")
+            print(data)
+            print("s03")
+            reply = json.loads(data)
+        except Exception as e:
+            print(type(e))
+            print(e.value)
+            print(e.__traceback__)
+            print(e)
+        print("s04")
         print(f"{datetime.datetime.now()} [紅月あい]: {reply}")
+        print("s05")
         data, rate = voicevox_adapter.get_voice(reply["character_reply"])
         play_sound.play_sound(data, rate)
 
