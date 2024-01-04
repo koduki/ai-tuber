bc6d7733c1f3853b8df175abeec63d1bf8007b15
koduki <pascalm3+git@gmail.com>
2024-01-05 04:55:05 +0900

    Refactoring

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ os.environ["GOOGLE_API_KEY"] = open("C:\\Users\\koduki\\.secret\\gemini.txt", "r
 # テンプレートとプロンプトエンジニアリング
 from langchain.prompts import (
     ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate
 )
-from typing import List
 
-from langchain.prompts import PromptTemplate
+prompt_system = open("prompt_system.txt", "r", encoding='utf-8').read()
+prompt = ChatPromptTemplate.from_messages([
+    ("system", prompt_system),
+    ("user", "{input}")
+])
+
+# モデルの準備
+from langchain.chat_models import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
+# llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+
+# 出力フォーマットを定義
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 
@@ -22,38 +32,15 @@ class Reply(BaseModel):
 
 parser = JsonOutputParser(pydantic_object=Reply)
 
-
-prompt_system = open("prompt_system.txt", "r", encoding='utf-8').read()
-
-# prompt = ChatPromptTemplate.from_messages([
-#     SystemMessagePromptTemplate.from_template(prompt_system),
-#     MessagesPlaceholder(variable_name="history"),
-#     HumanMessagePromptTemplate.from_template("{input}"),
-# ])
-
-from langchain.prompts import ChatPromptTemplate
-prompt = ChatPromptTemplate.from_messages([
-    ("system", prompt_system),
-    ("user", "{input}")
-])
-
-# チャットチェーンの準備
-from langchain.chains import ConversationChain
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.schema.messages import HumanMessage, SystemMessage
-
-# llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
-# llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
-
-# memory = ConversationBufferMemory(return_messages=True)
-# conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, partial_variables={"format_instructions": parser.get_format_instructions()})
+# チェインを作成
 chain = prompt | llm | parser
 
 
+
+
+#
+# Frontend
+#
 import pytchat
 
 # YouTubeのライブ配信またはプレミアム動画のURLまたは動画IDを設定
@@ -62,6 +49,7 @@ video_id = input() # "YOUR_VIDEO_ID"
 
 chat = pytchat.create(video_id=video_id)
 
+import time
 import json
 import datetime
 from voicevox_adapter import VoicevoxAdapter
@@ -74,26 +62,27 @@ print("Ready. Youtubeにコメントしてね。")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"{c.datetime} [{c.author.name}]: {c.message}")
-        res = chain.invoke({"input": c.message, "format_instructions": parser.get_format_instructions()})
-        print("s00")
-        print(res)
-        print("s01")
+        
         try:
-            # print(parser.parse(res))
-            print("s02")
-            data = str(res).replace("'", "\"")
-            print(data)
-            print("s03")
-            reply = json.loads(data)
-        except Exception as e:
-            print(type(e))
-            print(e.value)
-            print(e.__traceback__)
-            print(e)
-        print("s04")
-        print(f"{datetime.datetime.now()} [紅月あい]: {reply}")
-        print("s05")
-        data, rate = voicevox_adapter.get_voice(reply["character_reply"])
-        play_sound.play_sound(data, rate)
+            ls = time.perf_counter()
+            res = chain.invoke({"input": c.message, "format_instructions": parser.get_format_instructions()})
+            le = time.perf_counter()
+            print((le - ls))
 
+            print("res: " + str(res))
+
+            data = str(res).replace("'", "\"")
+            print("transformed: " + data)
+            reply = json.loads(data)
+            print("parsed: " + str(reply))
+    
+            print(f"{datetime.datetime.now()} [紅月あい]: {reply}")
+            ss = time.perf_counter()
+            data, rate = voicevox_adapter.get_voice(reply["character_reply"])
+            se = time.perf_counter()
+            print((se - ss))
+            play_sound.play_sound(data, rate)
+        except Exception as e:
+            print(e)
+            print(e.__traceback__)
 
