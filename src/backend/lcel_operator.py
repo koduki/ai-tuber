c08e1e3f28831c41953238f4f25b4ef6ae6ca875
koduki <pascalm3+git@gmail.com>
2024-04-07 12:45:46 +0900

    modify prompt for more general

--- src/backend/lcel_operator.py
+++ src/backend/lcel_operator.py
@@ -71,6 +71,7 @@ def _parse2json(json_text):
 def store_memory(memory):
     def f(response):
         # print(response)
+        print("raw_output: " + str(response["return_values"].return_values['output']))
         print("history: " + _format_messges(response['chat_history']))
 
         input = {"input":response["input"]}
@@ -83,32 +84,5 @@ def store_memory(memory):
 
 def to_json(response):
     import json
-    import re
-    text = response['output']
 
-    # parse non-JSON format
-    if "{" not in text and "}" not in text:
-        text = f'{{"current_emotion": "fun", "character_reply": "{text}"}}'
-
-    # parse include markdown
-    json_str = re.search(r'```json(.*?)```', text, re.DOTALL)
-    text = json_str.group(1) if json_str else text
-
-    # parse quartation
-    text = text.replace("'", "\"")
-
-    # transform and remove duplicate
-    #print(text)
-    split_output = text.split('\n')
-    unique_output = []
-    for item in split_output:
-        item_json = json.loads(item)
-        if item_json not in unique_output:
-            unique_output.append(item_json)
-
-    data = unique_output[0]
-
-    # parse broken current_emotion format
-    data["current_emotion"] = data["current_emotion"].split(":")[0]
-
-    return data
+    return json.loads(response['output'])
